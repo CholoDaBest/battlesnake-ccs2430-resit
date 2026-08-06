1580327bf94e4dc7bf99e499cc50d96e3a376170
Panagiotis <panosxologkitas@gmail.com>
2026-08-07 02:10:21 +0300

    Configure ESLint, Prettier, and EditorConfig

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
